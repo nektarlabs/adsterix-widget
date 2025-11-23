4366e44dae4a2085e96924478b0c954473230387
Alessandro Manfredi <alle.manfredi@gmail.com>
2025-11-23 07:06:48 +0100

    feat(playground): <- adds this

---
+++ playground/src/App.tsx
@@ -0,0 +1,10 @@
+import { AdsterixWidget } from "@nektarlabs/adsterix-widget"
+
+export function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Playground</h1>
+      <AdsterixWidget castHash="0xbf59074b94c5fd1c6b3ee1a7201708da3f60998f" />
+    </div>
+  )
+}
